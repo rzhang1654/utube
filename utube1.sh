504facaa4a35105eaaaea5d875296ae0240aee2d
rzhang1654 <run.zhang@exeloncorp.com>
2021-03-31 14:41:04 -0400

    Update utube1.sh

--- utube1.sh
+++ utube1.sh
@@ -1,1 +1,1 @@
-curl   'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&key=AIzaSyC1vlt5uPaE8BHOboBFj2lSKWuYoaPHmL4' --header 'Accept: application/json'   --compressed 
+curl   'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&key=' --header 'Accept: application/json'   --compressed 
