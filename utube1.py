24b7664b1644b8fab1b97d15445df64c0113fcf0
rzhang1654 <run.zhang@exeloncorp.com>
2021-03-31 14:40:10 -0400

    Update utube1.py

--- utube1.py
+++ utube1.py
@@ -7,7 +7,7 @@ except ImportError:
     print ("Please install the python-requests module.")
     sys.exit(-1)
 
-UB_API = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&key=AIzaSyC1vlt5uPaE8BHOboBFj2lSKWuYoaPHmL4"
+UB_API = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&key="
 SSL_VERIFY = True
 
 def get_json(url):
